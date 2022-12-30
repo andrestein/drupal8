147c3f4fbb5443bf6e2f118db4b469c523dacfa0
andres granda <andresgrandapiedrahita@gmail.com>
2022-12-30 12:28:12 -0500

    ajustando el xdebug

---
+++ web/modules/custom/hello_world/src/Controller/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\hello_world\Controller;
+
+class HelloController
+{
+    public function content()
+    {
+        return [
+            '#type' => 'markup',
+            '#markup' => t('Hello Drupal World!!!')
+        ];
+    }
+}
